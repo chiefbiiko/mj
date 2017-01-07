78f5d669a91edffb96d7008064a51dff55651a0f
Noah Schwarz <noah.anabiik.schwarz@gmail.com>
2017-01-07 20:38:08 +0100

    promised 2 deliver

--- mj.js
+++ mj.js
@@ -1,5 +1,8 @@
 /* mj - a js wrapper to the myjson.com api 4 simple persistent data storage
-only pass plain js objects to mj.create and mj.update..! */
+only pass plain js objects to mj.create and mj.update..! 
+mj.create, mj.update, and mj.get return promises, use like: 
+  mj.get("np94z").then(data => console.log(data)).catch(() => console.log('error'));
+   */
 'use strict';
 var mj = {
   downloadR: function(filename, text) {  // helpr that saves users myjson uri locally
@@ -17,48 +20,59 @@ var mj = {
     }
   },
   create: function(obj) {  // obj must be plain js object!
-    var xhr;
-    if (!window.confirm('aint yet got a store/uri? check mj.uri or in ur download dir "mj_uri.txt" for an existing uri. continue anyways?')) {
-      return false;  // exit on cancel
-    }
-    xhr = new XMLHttpRequest();
-    xhr.open('POST', 'https://api.myjson.com/bins', true);
-    xhr.setRequestHeader('Content-Type', 'application/json');
-    xhr.setRequestHeader('Accept', 'application/json');
-    xhr.addEventListener('load', function(e) {
-      mj.uri = JSON.parse(this.responseText).uri;  // mj.uri always holds the last utilized uri during a session
-      mj.downloadR('mj_uri.txt', mj.uri);
-      window.alert(mj.uri + ' | saved this uri. u will need the id after "bins/" 2 get and update your json.');
+    return new Promise(function(resolve, reject) {
+      var xhr;
+      if (!window.confirm('aint yet got a store/uri? check mj.uri or in ur download dir "mj_uri.txt" for an existing uri. continue anyways?')) {
+        return Promise.reject(new Error('cancel'));  // exit on cancel
+      }
+      xhr = new XMLHttpRequest();
+      xhr.onerror = reject;
+      xhr.open('POST', 'https://api.myjson.com/bins', true);
+      xhr.setRequestHeader('Content-Type', 'application/json');
+      xhr.setRequestHeader('Accept', 'application/json');
+      xhr.addEventListener('load', function(e) {
+        mj.uri = JSON.parse(this.responseText).uri;  // mj.uri always holds the last utilized uri during a session
+        mj.downloadR('mj_uri.txt', mj.uri);
+        window.alert(mj.uri + ' | saved this uri. u will need the id after "bins/" 2 get and update your json.');
+        resolve(mj.uri);
+      });
+      xhr.send(JSON.stringify(obj));
     });
-    xhr.send(JSON.stringify(obj));
   },
-  update: function(id, obj) {  // id can either be the entire uri or just ur id  // obj must b plain js object!  
-    var uri, xhr;
-    (id.includes('https:')) ? uri = id : uri = 'https://api.myjson.com/bins/' + id;
-    mj.uri = uri.replace(/^.*bins\//, '');
-    xhr = new XMLHttpRequest();
-    xhr.open('PUT', uri, true);
-    xhr.setRequestHeader('Content-Type', 'application/json');
-    xhr.setRequestHeader('Accept', 'application/json');
-    xhr.addEventListener('load', function(e) {
-      var data = JSON.parse(this.responseText);
-      console.log(data);
-      mj.data = data;  // mj.data is local store of ur data during a session
+  update: function(id, obj) {  // id can either be the entire uri or just ur id  // obj must b plain js object!
+    return new Promise(function(resolve, reject) {
+      var uri, xhr;
+      (id.includes('https:')) ? uri = id : uri = 'https://api.myjson.com/bins/' + id;
+      mj.uri = uri.replace(/^.*bins\//, '');
+      xhr = new XMLHttpRequest();
+      xhr.onerror = reject;
+      xhr.open('PUT', uri, true);
+      xhr.setRequestHeader('Content-Type', 'application/json');
+      xhr.setRequestHeader('Accept', 'application/json');
+      xhr.addEventListener('load', function(e) {
+        var data = JSON.parse(this.responseText);
+        console.log(data);
+        mj.data = data;  // mj.data is local store of ur data during a session
+        resolve(data);
+      });
+      xhr.send(JSON.stringify(obj));
     });
-    xhr.send(JSON.stringify(obj));
   },
   get: function(id) {  // id can either be the entire uri or just ur id
-    var xhr;
-    (id.includes('bins/')) ? mj.id = id.replace(/^.*bins\//, '') : mj.id = id;
-    xhr = new XMLHttpRequest();
-    xhr.open('GET', 'https://api.myjson.com/bins/' + mj.id, true);
-    xhr.setRequestHeader('Content-Type', 'application/json');
-    xhr.setRequestHeader('Accept', 'application/json');
-    xhr.addEventListener('load', function(e) {
-      var data = JSON.parse(this.responseText);
-      console.log(data);
-      mj.data = data;
+    return new Promise(function(resolve, reject) {
+      var xhr;
+      (id.includes('bins/')) ? mj.id = id.replace(/^.*bins\//, '') : mj.id = id;
+      xhr = new XMLHttpRequest();
+      xhr.onerror = reject;
+      xhr.open('GET', 'https://api.myjson.com/bins/' + mj.id, true);
+      xhr.setRequestHeader('Content-Type', 'application/json');
+      xhr.setRequestHeader('Accept', 'application/json');
+      xhr.addEventListener('load', function(e) {
+        var data = JSON.parse(this.responseText);
+        mj.data = data;
+        resolve(data);
+      });
+      xhr.send();
     });
-    xhr.send();
   }
 };
