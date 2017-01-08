bac0c327371664181b803be343b4d6d0f195d524
Noah Schwarz <noah.anabiik.schwarz@gmail.com>
2017-01-08 13:44:02 +0100

    added docs

--- mj.js
+++ mj.js
@@ -1,28 +1,23 @@
-/* mj - a js wrapper to the myjson.com api 4 simple persistent data storage
-only pass plain js objects 2 mj.create n mj.update..! 1st arg of mj.update n mj.get can be entire uri or just id
-mj.create, mj.update, and mj.get return promises, use like:
-  mj.create({tbl1: [[]]}).then(uri => console.log(`${uri} : saved uri in mj.uri`));
-  mj.update(mj.uri, {tbl1: [[]], tbl2: [[]]}).then(data => console.log(data));
-  mj.get('np419y').then(data => console.log(data)); */
+/** mj - a js wrapper to the myjson.com API 4 simple persistent data storage
+ *  Examples:
+ *    mj.create({tbl1: [[]]}).then(uri => console.log(`${uri} : saved URI in mj.uri`));
+ *    mj.update(mj.uri, {tbl1: [[]], tbl2: [[]]}).then(data => console.log(data));
+ *    mj.get('np419y').then(data => console.log(data));
+**/
 'use strict';
 var mj = {
-  uri: '',
-  data: {},
-  saveUri(filename, text) {  // helpr that saves users myjson uri locally
-    var blob = new Blob([text], {type: 'text/plain'});
-    var a = window.document.createElement('a');
-    a.href = window.URL.createObjectURL(blob);
-    a.download = filename;
-    document.body.appendChild(a);
-    a.click();
-    document.body.removeChild(a);
-    window.URL.revokeObjectURL(a.href);
-  },
-  create(obj) {  // obj must be plain js object!
+  uri: '',  // always holds the last utilized URI during a session
+  data: {},  // always holds the last resolved data during a session
+  /**
+   *  Creates a new store
+   *  @param {Object} obj Plain js object !NOT JSON!
+   *  @return {Promise.<string>} URI
+  **/
+  create(obj) { 
     return new Promise(function(resolve, reject) {
       var xhr;
-      if (!obj || !window.confirm('aint yet got a store/uri? check mj.uri or in ur download dir "mj_uri.txt". continue anyways?')) {
-        return reject('missing input?');  // exit on cancel
+      if (!obj || !window.confirm('aint yet got a store/URI? check mj.uri or in ur download dir "mjuri.txt". continue anyways?')) {
+        return reject('cancel');  // exit
       }
       xhr = new XMLHttpRequest();
       xhr.onerror = reject;
@@ -30,19 +25,25 @@ var mj = {
       xhr.setRequestHeader('Content-Type', 'application/json');
       xhr.setRequestHeader('Accept', 'application/json');
       xhr.addEventListener('load', function(e) {
-        mj.uri = JSON.parse(this.responseText).uri;  // mj.uri always holds the last utilized uri during a session
-        mj.saveUri('mj_uri.txt', mj.uri);
-        window.alert(`${mj.uri} : saved this uri. u will need it 2 get and update your json.`);
+        mj.uri = JSON.parse(this.responseText).uri;
+        mj._saveUri('mjuri.txt', mj.uri);
+        window.alert(`${mj.uri} : saved this URI. u will need it 2 get and update ur store.`);
         resolve(mj.uri);
       });
       xhr.send(JSON.stringify(obj));
     });
   },
-  update(id, obj) {  // id can either be the entire uri or just ur id  // obj must b plain js object!
+  /**
+   *  Updates an existing store
+   *  @param {string} id Either entire URI or just an id
+   *  @param {Object} obj Plain js object !NOT JSON!
+   *  @return {Promise.<Object>} Plain js object
+  **/
+  update(id, obj) {
     return new Promise(function(resolve, reject) {
       var xhr;
       if (!id || !obj) {
-        return reject('missing input!');
+        return reject('no input!');
       }
       (/https:\/\/api.myjson.com\/bins\//.test(id)) ? mj.uri = id : mj.uri = `https://api.myjson.com/bins/${id}`;
       xhr = new XMLHttpRequest();
@@ -52,17 +53,22 @@ var mj = {
       xhr.setRequestHeader('Accept', 'application/json');
       xhr.addEventListener('load', function(e) {
         var data = JSON.parse(this.responseText);
-        mj.data = data;  // mj.data is local store of ur data during a session
+        mj.data = data;
         resolve(data);
       });
       xhr.send(JSON.stringify(obj));
     });
   },
-  get(id) {  // id can either be the entire uri or just ur id
+  /**
+   *  Gets data from a store
+   *  @param {string} id Either entire URI or just an id
+   *  @return {Promise.<Object>} Plain js object
+  **/
+  get(id) {
     return new Promise(function(resolve, reject) {
       var urid, xhr;  // urid is used to always extract id, then build full uri, ensuring https
       if (!id) {
-        return reject('missing input!');
+        return reject('no input!');
       }
       (id.includes('bins/')) ? urid = id.replace(/^.*bins\//, '') : urid = id;
       xhr = new XMLHttpRequest();
@@ -77,5 +83,18 @@ var mj = {
       });
       xhr.send();
     });
+  },
+  /**
+   *  Saves users myjson URI locally
+  **/ 
+  _saveUri(filename, text) {
+    var blob = new Blob([text], {type: 'text/plain'});
+    var a = window.document.createElement('a');
+    a.href = window.URL.createObjectURL(blob);
+    a.download = filename;
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+    window.URL.revokeObjectURL(a.href);
   }
 };
