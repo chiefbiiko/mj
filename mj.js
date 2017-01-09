4e0e7e749c71f7355867dec6ede39e89e7673c35
Noah Schwarz <noah.anabiik.schwarz@gmail.com>
2017-01-09 16:27:20 +0100

    passing thru xhr.status on reject

--- mj.js
+++ mj.js
@@ -16,26 +16,9 @@ var mj = {
       xhr.open('POST', 'https://api.myjson.com/bins', true);
       xhr.setRequestHeader('Content-Type', 'application/json');
       xhr.setRequestHeader('Accept', 'application/json');
-      xhr.addEventListener('load', e => xhr.status === 200 ? resolve(mj.uri = JSON.parse(xhr.responseText).uri) : reject('xhr error'));
-      xhr.send(JSON.stringify(obj));
-    });
-  },
-  /**
-   *  Updates an existing store
-   *  @param {string} id Either entire URI or just an id
-   *  @param {Object} obj Plain js object !NOT JSON!
-   *  @return {Promise.<Object>} Plain js object
-  **/
-  update(id, obj) {
-    return new Promise((resolve, reject) => {
-      var xhr;
-      (!id || !obj) && reject('no input!');
-      /https:\/\/api.myjson.com\/bins\//.test(id) ? mj.uri = id : mj.uri = `https://api.myjson.com/bins/${id}`;
-      xhr = new XMLHttpRequest();
-      xhr.open('PUT', mj.uri, true);
-      xhr.setRequestHeader('Content-Type', 'application/json');
-      xhr.setRequestHeader('Accept', 'application/json');
-      xhr.addEventListener('load', e => xhr.status === 200 ? resolve(mj.data = JSON.parse(xhr.responseText)) : reject('xhr error'));
+      xhr.addEventListener('load', e => {
+        xhr.status === 200 ? resolve(mj.uri = JSON.parse(xhr.responseText).uri) : reject(`xhr error ${xhr.status}`);
+      });
       xhr.send(JSON.stringify(obj));
     });
   },
@@ -53,8 +36,31 @@ var mj = {
       xhr.open('GET', mj.uri = `https://api.myjson.com/bins/${urid}`, true);
       xhr.setRequestHeader('Content-Type', 'application/json');
       xhr.setRequestHeader('Accept', 'application/json');
-      xhr.addEventListener('load', e => xhr.status === 200 ? resolve(mj.data = JSON.parse(xhr.responseText)) : reject('xhr error'));
+      xhr.addEventListener('load', e => {
+        xhr.status === 200 ? resolve(mj.data = JSON.parse(xhr.responseText)) : reject(`xhr error ${xhr.status}`);
+      });
       xhr.send();
+    });
+  },
+  /**
+   *  Updates an existing store
+   *  @param {string} id Either entire URI or just an id
+   *  @param {Object} obj Plain js object !NOT JSON!
+   *  @return {Promise.<Object>} Plain js object
+  **/
+  update(id, obj) {
+    return new Promise((resolve, reject) => {
+      var xhr;
+      (!id || !obj) && reject('no input!');
+      /https:\/\/api.myjson.com\/bins\//.test(id) ? mj.uri = id : mj.uri = `https://api.myjson.com/bins/${id}`;
+      xhr = new XMLHttpRequest();
+      xhr.open('PUT', mj.uri, true);
+      xhr.setRequestHeader('Content-Type', 'application/json');
+      xhr.setRequestHeader('Accept', 'application/json');
+      xhr.addEventListener('load', e => {
+        xhr.status === 200 ? resolve(mj.data = JSON.parse(xhr.responseText)) : reject(`xhr error ${xhr.status}`); 
+      });
+      xhr.send(JSON.stringify(obj));
     });
   }
 };
